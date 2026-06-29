b7d6428784612fed059d620744a18a377d84643a
ManhattanCafe2002 <manhattancafe20020428@gmail.com>
2026-06-29 22:24:07 +0900

    myfirst_cpp

---
+++ CPP_basic/myfirst.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main()
+{
+    using namespace std;    //make difination of cout and endl available
+    cout << "Hello, World!" << endl;
+    cout << "This is my first C++ program." << endl;
+    return 0;               // as same as c       
+}
