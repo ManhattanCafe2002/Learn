507cd855aa9beda79953567269aedb2add8d501e
ManhattanCafe2002 <manhattancafe20020428@gmail.com>
2026-08-19 05:38:07 +0900

    using namespace std::cout;

--- CPP_basic/myfirst.cpp
+++ CPP_basic/myfirst.cpp
@@ -3,6 +3,10 @@
 int main()
 {
     using namespace std;    //make difination of cout and endl available
+    //OR
+    //using std::cout;
+    //using std::endl;
+    //using std::cin;
     cout << "Hello, World!" << endl;
     cout << "This is my first C++ program." << endl;
     cin.get();                // wait for user to press enter
