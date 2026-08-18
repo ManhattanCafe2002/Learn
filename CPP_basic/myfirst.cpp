3330ae4d259bafeb54036e8f744173bade7d6176
ManhattanCafe2002 <manhattancafe20020428@gmail.com>
2026-08-19 06:28:59 +0900

    practice cin cout

--- CPP_basic/myfirst.cpp
+++ CPP_basic/myfirst.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 
+double multiple2(double);
+
 int main()
 {
     using namespace std;    //make difination of cout and endl available
@@ -7,8 +9,18 @@ int main()
     //using std::cout;
     //using std::endl;
     //using std::cin;
+    int days = 7;
+    int day;
     cout << "Hello, World!" << endl;
     cout << "This is my first C++ program." << endl;
+    cout << "There are " << days << " days in a week." << endl;
+    cout << "today is the day";
+    cin >> day;
+    cout <<"day"<< multiple2(day) <<" is a good day."<< endl;
     cin.get();                // wait for user to press enter
     return 0;               // as same as c       
+}
+double mutiple2(double x)
+{
+    return x * 2;
 }
