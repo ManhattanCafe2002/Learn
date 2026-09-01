46d2d146aeb9c1c665f0b67d1c064be4825caefa
ManhattanCafe2002 <manhattancafe20020428@gmail.com>
2026-09-01 21:54:47 +0900

    struct union enum

--- CPP_basic/tryclass.cpp
+++ CPP_basic/tryclass.cpp
@@ -31,3 +31,32 @@ class Vector
 
 
 //double& operator[](int i){ return elem[i];}
+
+union Value
+{
+    int i;
+    double d;
+    char c;
+};
+// The union Value can hold either an int, a double, or a char, but only one of them at a time.
+enum class Color {red=0, green=1, blue=2};
+enum class Traffic_light {green=0, yellow=1, red=2};
+
+Color col = Color::red;
+Traffic_light light = Traffic_light::red;
+// The above code defines two enumerations: Color and Traffic_light.
+// Color x = red; // This is not allowed because Color and Traffic_light are distinct types.
+// Color y = Traffic_light::red; // This is also not allowed for the same reason.
+Traffic_light& operator++(Traffic_light& t)
+{
+    switch(t)
+    {
+        case Traffic_light::green:
+            return t = Traffic_light::yellow;
+        case Traffic_light::yellow:
+            return t = Traffic_light::red;
+        case Traffic_light::red:
+            return t = Traffic_light::green;
+    }
+}
+Traffic_light next = ++light; // This will change light from red to green, and next will also be green.
